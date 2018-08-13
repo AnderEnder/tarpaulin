1d453c8e761efdb956fb6086ebfbb441075e82cf
Andrii Radyk <andrii.radyk@sizmek.com>
2018-08-13 22:50:40 +0200

    update release

---
+++ ci/before_deploy2.sh
@@ -0,0 +1,31 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    # TODO Update this to build the artifacts that matter to you
+    cargo build --release 
+
+    # TODO Update this to package the right artifacts
+    cp target/release/cargo-tarpaulin $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_RUST_VERSION-$TRAVIS_TAG-travis.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
