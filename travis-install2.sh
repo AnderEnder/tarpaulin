2290bd38fa44d0e082fb12d172966f9d4497bf4a
Andrii Radyk <andrii.radyk@sizmek.com>
2018-08-14 00:58:27 +0200

    add shell script to install

--- travis-install2.sh
+++ travis-install2.sh
@@ -2,7 +2,7 @@
 TARPAULIN_VERSION=${TARPAULIN_VERSION:-0.6.6-1}
 RUST_VERSION=$(rustc -V | cut -d' ' -f 2)
 
-if curl -sL --fail -O cargo-tarpaulin.tar.gz https://github.com/AnderEnder/tarpaulin/releases/download/${TARPAULIN_VERSION}/cargo-tarpaulin-${RUST_VERSION}-${TARPAULIN_VERSION}-travis.tar.gz; then
+if curl -L --fail -O cargo-tarpaulin.tar.gz https://github.com/AnderEnder/tarpaulin/releases/download/${TARPAULIN_VERSION}/cargo-tarpaulin-${RUST_VERSION}-${TARPAULIN_VERSION}-travis.tar.gz; then
     tar xvz -C $HOME/.cargo/bin cargo-tarpaulin.tar.gz
 else
     RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install cargo-tarpaulin
